96f2d3d25680a3d39771abc0c112997bdd647503
Saad-Rahman-Warsi <113453248+Saad-Rahman-Warsi@users.noreply.github.com>
2023-12-12 00:59:28 -0500

    Update get-service.service.ts

--- src/app/get-service.service.ts
+++ src/app/get-service.service.ts
@@ -7,7 +7,7 @@ import { Respose } from './model/Response';
   providedIn: 'root'
 })
 export class GetServiceService {
-  url:string="http://apigateway-route-saadrahmanwarsi-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/tibua/"
+  url:string="http://ocbustracker-58-saadrahmanwarsi-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/viewbus/"
   constructor(private http: HttpClient) { }
   getEmployeeById()
   {
