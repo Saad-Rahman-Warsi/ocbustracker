5c1ca311b974c3e23133b0c66e07ec2e2f490c63
Saad-Rahman-Warsi <113453248+Saad-Rahman-Warsi@users.noreply.github.com>
2023-12-13 03:34:35 -0500

    Update postservice.service.ts

--- src/app/postservice.service.ts
+++ src/app/postservice.service.ts
@@ -9,13 +9,13 @@ import { HttpClient,HttpErrorResponse,HttpHeaders,HttpParams } from '@angular/co
 export class PostserviceService {
 
   httpParams: HttpParams = new HttpParams();
-  url:any="http://sendpost-route-saadrahmanwarsi-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com";
+  url_b:any="http://sendpost-route-saadrahmanwarsi-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com";
   constructor(private http: HttpClient) { }
 
   save(station:number,route:number)
   {
-    this.url=this.url+"/"+station+"/"+route;
-    return this.http.get<any>(this.url);    
+    url:any=this.url_b+"/"+station+"/"+route;
+    return this.http.get<any>(url);    
     
    
   }
